c99a27bc5bb338ea2ca1fc2d4568bc42335f1e9e
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-09-20 15:54:45 -0400

    varibales

--- modern-javascript/Project-Files/ModernJavascript.js
+++ modern-javascript/Project-Files/ModernJavascript.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+// console.log("Hello World")
+
+// var age = 26;
+// age = 27
+// age = age + 1
+
+// var health = 0.65;
+
+// var isAlive = true;
+// isAlive = false;
+
+// isAlive = 5 > 2
+
+// const pi = 3.14159
+// let numberOfLimbs = 4
+
