fe65deb6b61c8f53d31cf19417863efd0f03c89a
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-09-20 16:02:02 -0400

    string variables

--- modern-javascript/Project-Files/ModernJavascript.js
+++ modern-javascript/Project-Files/ModernJavascript.js
@@ -1,5 +1,7 @@
 // console.log("Hello World")
 
+//  VARIABLES
+
 // var age = 26;
 // age = 27
 // age = age + 1
@@ -13,4 +15,13 @@
 
 // const pi = 3.14159
 // let numberOfLimbs = 4
+
+// STRING VARIABLES
+
+// var name = "Doug";
+// var zenva = "Zenva";
+
+// let concat = name + zenva
+
+// let finalStr = `Hi! My name is ${name} and I'm taking a course on ${zenva}`
 
