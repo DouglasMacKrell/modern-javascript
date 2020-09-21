bd7f4ebd62f9e823f882e85ffb99a5c44a7e2c24
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-09-21 19:27:01 -0400

    async and await

--- modern-javascript/Project-Files/ModernJavascript.js
+++ modern-javascript/Project-Files/ModernJavascript.js
@@ -76,3 +76,17 @@
 //   return await num1 + num2; // fetch data here
 // }
 
+// FETCHING DATA FROM AN URL
+
+const fetch = require('node-fetch');
+
+let url =
+  "http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=221380";
+
+async function fetchData(url) {
+    let response = await fetch(url);
+    let jsonResponse = await response.json();
+    console.log(JSON.stringify(jsonResponse));
+}
+
+fetchData(url).catch(() => { console.log("Oops, all errors!") })
